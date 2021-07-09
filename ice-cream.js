1d19846c7b02ac6f807ec3856c5c8a94a8629a10
Arushi Katyal <katy.arushi@gmail.com>
2021-07-09 01:17:18 +0000

    prep

---
+++ ice-cream.js
@@ -0,0 +1,12 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+let newArray = iceCreamFlavours.slice(0, iceCreamFlavours.length)
+newArray.push("root beer") 
+console.log(newArray);
+
+console.log(iceCreamFlavours[0]);
+
+console.log(newArray[5]);
+
+console.log(newArray.length);
